07f0296ca71e26570c347463b898587076432e81
HP <micaa8264@gmail.com>
2025-02-16 15:53:31 +0700

    Finish

---
+++ app/src/main/java/com/example/tokodonat/detail_produk4.java
@@ -0,0 +1,14 @@
+package com.example.tokodonat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detail_produk4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_produk4);
+    }
+}
