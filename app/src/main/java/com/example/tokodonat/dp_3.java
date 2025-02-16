8b2de62ade50890a9440750fda4d75223069524a
HP <micaa8264@gmail.com>
2025-02-16 16:11:25 +0700

    Finish

--- app/src/main/java/com/example/tokodonat/dp_3.java
+++ app/src/main/java/com/example/tokodonat/dp_3.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class detail_produk3 extends AppCompatActivity {
+public class dp_3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_produk3);
+        setContentView(R.layout.activity_dp3);
     }
 }
